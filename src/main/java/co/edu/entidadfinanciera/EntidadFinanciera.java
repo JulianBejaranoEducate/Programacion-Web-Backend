98980e5ee75945968a2405ab7de558ca3c60c41d
Canquito <jbejarano@uniempresarial.edu.co>
2024-11-17 14:45:34 -0500

    New change

--- src/main/java/co/edu/entidadfinanciera/EntidadFinanciera.java
+++ src/main/java/co/edu/entidadfinanciera/EntidadFinanciera.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EntidadFinanciera {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EntidadFinanciera.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EntidadFinanciera.class, args);
+    }
+    // This a new message
 }
